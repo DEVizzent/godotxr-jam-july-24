fa6a7f932652b22cf73a113d6c952bbde1a83f58
Vicent Valls <vizzent@gmail.com>
2024-07-12 17:00:25 +0200

    Implement reset canicas button

---
+++ scenes/Elements/cajon.gd
@@ -0,0 +1,24 @@
+extends StaticBody3D
+
+var posicionOrigenCanica1 : Vector3
+var posicionOrigenCanica2 : Vector3
+var posicionOrigenCanica3 : Vector3
+@onready var canica1 : XRToolsPickable = $PickableCanica
+@onready var canica2 : XRToolsPickable = $PickableCanica2
+@onready var canica3 : XRToolsPickable = $PickableCanica3
+# Called when the node enters the scene tree for the first time.
+func _ready() -> void:
+	posicionOrigenCanica1 = canica1.global_position
+	posicionOrigenCanica2 = canica2.global_position
+	posicionOrigenCanica3 = canica3.global_position
+
+
+
+func _on_reset_button_released(button: Variant) -> void:
+	canica1.global_position = posicionOrigenCanica1
+	canica2.global_position = posicionOrigenCanica2
+	canica3.global_position = posicionOrigenCanica3
+
+
+func _on_reset_message_showed() -> void:
+	$BotonReinicio.visible = true
--- scenes/textocartel.gd
+++ scenes/textocartel.gd
@@ -1,11 +1,21 @@
 extends Label3D
 
+var ballCounter:int = 0
+
+signal reset_message_showed()
+
 func _ready():
 	cambioTexto("Take a ball! \n⬇",70)
 
-func cambioTexto(texto, tamano):
+func cambioTexto(texto, tamano)->void:
 	text = str(texto)
 	font_size = tamano
 
 func _on_pickable_canica_picked_up(pickable: Variant) -> void:
-	cambioTexto("Throw it making the green\none fall out of the platform\n⬅",40)
+	ballCounter += 1
+	if(ballCounter == 1):
+		cambioTexto("Throw it making the colored\nones fall out of the platform\n⬅",40)
+	if (ballCounter == 3):
+		cambioTexto("Pulse red button '\nto get the balls back\n⬇",50)
+		reset_message_showed.emit()
+		
